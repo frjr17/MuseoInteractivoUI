fae5844a84be49d8210aa0b4ffa42de8d5cee679
Hernán Valencia <hernanadrianv17@gmail.com>
2025-10-31 11:19:28 -0500

    implmementing VITE_API_URL variable

--- vite.config.ts
+++ vite.config.ts
@@ -2,21 +2,26 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 import path from 'path'
+import { loadEnv } from 'vite'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react(), tailwindcss()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "./src"),
+export default defineConfig(({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), '')
+  const API_URL = env.VITE_API_URL || process.env.VITE_API_URL
+  return {
+    plugins: [react(), tailwindcss()],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "./src"),
+      },
     },
-  },
-  server:{
-    proxy:{
-      '/api':{
-        target: 'http://localhost:5000',
-        changeOrigin: true,
-        rewrite: (path) => path.replace(/^\/api/, '/'),
+    server: {
+      proxy: {
+        '/api': {
+          target: API_URL,
+          changeOrigin: true,
+          rewrite: (path) => path.replace(/^\/api/, '/'),
+        }
       }
     }
   }

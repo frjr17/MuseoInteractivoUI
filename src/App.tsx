7b6ddd8514dff88d248af2a0b0d912af88e3182e
Hernán Valencia <hernanadrianv17@gmail.com>
2025-10-31 15:56:51 -0500

    ✨ feat: routing user profile page

--- src/App.tsx
+++ src/App.tsx
@@ -2,12 +2,14 @@ import { Route, Routes } from "react-router";
 import LoginPage from "./pages/LoginPage";
 import { ForgotPassword } from "./pages/ForgotPasswordPage";
 import { MuseumHome } from "./pages/MuseumHome";
+import { UserProfile } from "./pages/UserProfilePage";
 
 
 function App() {
   return (
     <Routes>
       <Route path="/" element={<MuseumHome />} />
+      <Route path="/profile" element={<UserProfile />} />
       <Route path="/sign" element={<LoginPage />} />
       <Route path="/forgot-password/*" element={<ForgotPassword />} />
     </Routes>
